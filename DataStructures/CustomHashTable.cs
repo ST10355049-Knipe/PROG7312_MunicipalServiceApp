7e1b947e6a08d609cad06e6ee3caf20d0fafbbf2
Liam Knipe <129791138+ST10355049-Knipe@users.noreply.github.com>
2025-11-10 15:28:34 +0200

    Lecturers Feedback Implementation
    - Built a simple Hash Table which will use an array for storage ( Lecturers Feedback)
    
    - Built a simple Stack that uses my CustomLinkedList interanally to store the items ( Lecturers Feedback)

---
+++ DataStructures/CustomHashTable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PROG7312_MunicipalServiceApp.DataStructures
+{
+
+    public class CustomHashTable<TKey, TValue>
+    {
+        // The size of the internal array. A prime number is often good for distribution.
+        private const int InitialCapacity = 17;
+        private readonly CustomLinkedList<KeyValuePair<TKey, TValue>>[] buckets;
+
+        public CustomHashTable()
+        {
+            buckets = new CustomLinkedList<KeyValuePair<TKey, TValue>>[InitialCapacity];
+        }
+
+        // A simple hash function to map a key to an index in our array.
+        private int GetBucketIndex(TKey key)
+        {
+            // We use absolute value to ensure the index is never negative.
+            int hashCode = key.GetHashCode();
+            int index = Math.Abs(hashCode) % buckets.Length;
+            return index;
+        }
+
+        // Adds a key-value pair to the hash table.
+        public void Add(TKey key, TValue value)
+        {
+            int index = GetBucketIndex(key);
+
+            // If this bucket is empty, initialize a new linked list for it.
+            if (buckets[index] == null)
+            {
+                buckets[index] = new CustomLinkedList<KeyValuePair<TKey, TValue>>();
+            }
+
+            buckets[index].Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        // Tries to find a value associated with the given key.
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            int index = GetBucketIndex(key);
+            var bucket = buckets[index];
+
+            if (bucket != null)
+            {
+                var current = bucket.head;
+                while (current != null)
+                {
+                    if (current.Data.Key.Equals(key))
+                    {
+                        value = current.Data.Value;
+                        return true; // Found it!
+                    }
+                    current = current.Next;
+                }
+            }
+
+            value = default(TValue);
+            return false; // Not found.
+        }
+    }
+}
---
+++ DataStructures/CustomStack.cs
@@ -0,0 +1,36 @@
+﻿namespace PROG7312_MunicipalServiceApp.DataStructures
+{
+    // A standard LIFO (Last-In, First-Out) stack.
+    // I'm using my CustomLinkedList internally to store the items.
+    public class CustomStack<T>
+    {
+        private readonly CustomLinkedList<T> items;
+
+        public CustomStack()
+        {
+            items = new CustomLinkedList<T>();
+        }
+
+        // Pushes a new item onto the top of the stack.
+        public void Push(T item)
+        {
+            // Adding to the front of the linked list makes it behave like a stack.
+            items.AddFirst(item);
+        }
+
+        // Peeks at the item on top of the stack without removing it.
+        public T Peek()
+        {
+            if (items.head == null)
+            {
+                return default(T); // Stack is empty.
+            }
+            return items.head.Data;
+        }
+
+        public bool IsEmpty()
+        {
+            return items.head == null;
+        }
+    }
+}
